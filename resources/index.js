db1db8039f141f6d90cf08f98e5958b11953194d
Jason Keller <kellej6@ucmail.uc.edu>
2020-11-02 20:30:04 -0500

    added timer logic to index.js

--- resources/index.js
+++ resources/index.js
@@ -2,20 +2,40 @@ let start = document.getElementById("startButton");
 let canvas = document.getElementById("matchItCanvas");
 let countryName = document.getElementById("country");
 
+// timer
+let countDownDate = new Date();
+countDownDate.setMinutes(countDownDate.getMinutes() + 2);
 
-try{
+let countDown = setInterval(function () {
+
+    let now = new Date().getTime();
+    let timeDistance = countDownDate - now;
+
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000) * minutes;
+
+    document.getElementById("timer").innerHTML = seconds + "s";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+
+}, 1000);
+
+try {
 
 
     let game = new MatchIt(canvas)
 
-    start.addEventListener("click", function(){
+    start.addEventListener("click", function () {
         event.preventDefault();
-        game.start().then(()=>{
-        countryName.innerHTML = "Country name: " + game.country;
+        game.start().then(() => {
+            countryName.innerHTML = "Country name: " + game.country;
         });
     });
 
-} catch(error){
+} catch (error) {
     console.error(error);
     alert(error);
 }
