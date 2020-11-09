c573753c07dcd73d8a94edf3ec299e96ce3a670a
Cheghali Elkhalili <Cheghali Elkhalili>
2020-11-09 00:10:02 -0500

    fixed code error

--- resources/index.js
+++ resources/index.js
@@ -3,30 +3,31 @@ let canvas = document.getElementById("matchItCanvas");
 let countryName = document.getElementById("country");
 let guessButton = document.getElementById("guess");
 let playAgainButton = document.getElementById("resetGame");
-
+//var interval = setInterval(startTimer, 1000);
 let countDownDate = new Date();
 countDownDate.setMinutes(countDownDate.getMinutes() + 2);
-
+//var interval;
 function startTimer() {
     let now = new Date().getTime();
     let timeDistance = countDownDate - now;
 
     var minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeDistance % (1000 * 60)) / 1000);
-
+    
     document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s";
 
     if (timeDistance < 0) {
         clearInterval(x);
         document.getElementById("timer").innerHTML = "EXPIRED";
     }
+
 };
 
 try {
 
     let game = new MatchIt(canvas)
 
-    start.addEventListener("click", function () {
+    start.addEventListener("click", function (e) {
         event.preventDefault();
         game.start().then(() => {
             countryName.innerHTML = "Country name: " + game.country;
@@ -35,11 +36,17 @@ try {
             guessButton.disabled = false;
             playAgainButton.disabled = false;
 
-        // start the timer
-        setInterval(startTimer, 1000);
-    });
+            
 
-} catch (error) {
+        // start the timer
+        
+        setInterval(startTimer, 1000)
+    })   
+    });     
+
+     } catch (error) {
     console.error(error);
     alert(error);
 }
+ 
+
