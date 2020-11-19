167f32d224d52c90352cd3c4d0d61a74d3cd2971
Jason Keller <kellej6@ucmail.uc.edu>
2020-11-19 16:46:57 -0500

    timer functionality modifications
    I corrected the timer function and included the 30 second decrement for each addition 10 wins.

--- resources/index.js
+++ resources/index.js
@@ -5,27 +5,70 @@ let guessButton = document.getElementById("guessSubmitButton");
 let playAgainButton = document.getElementById("resetGame");
 let capital = document.getElementById("capital");
 
+var timer;  // the timer function
+var twoMinutesFromNow; // the time two minutes from now
+var win_count = 0; //  the count of times the player has won
 
+function countdown() {
+  // the time as of right now
+  var rightnow = new Date().getTime();
 
-let countDownDate = new Date();
-countDownDate.setMinutes(countDownDate.getMinutes() + 2);
+  // the time differential between right now and two minutes from now
+  var distance = twoMinutesFromNow - rightnow;
+  console.log(distance);
 
-function startTimer() {
-  let now = new Date().getTime();
-  let timeDistance = countDownDate - now;
+  // the time expansion into units of time
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  let minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((timeDistance % (1000 * 60)) / 1000);
+  // format the time that will display on the board
+  var text = minutes + "m " + seconds + "s ";
+  document.getElementById("timer").innerHTML = text;
 
-  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s";
-
-  if (timeDistance < 0) {
-    clearInterval(x);
+  // if the timer runs out, display "expired" and stop the timer
+  if (distance < 0) {
     document.getElementById("timer").innerHTML = "EXPIRED";
-    playAgainButton.disabled = false;
+    clearInterval(timer);
   }
+}
 
-};
+// set the variable "twoMinutesFromNow" to two minutes and two seconds from right now
+function setTimer() {
+  twoMinutesFromNow = new Date();
+  twoMinutesFromNow.setMinutes(twoMinutesFromNow.getMinutes() + 2);
+  twoMinutesFromNow.setSeconds(twoMinutesFromNow.getSeconds() + 2);
+
+  console.log(win_count);
+
+  // decrement the timer by 30 seconds for each 10 additional wins
+  switch (win_count) {
+    case 10:
+      twoMinutesFromNow.setSeconds(twoMinutesFromNow.getSeconds() - 30);
+      break;
+
+    case 20:
+      twoMinutesFromNow.setSeconds(twoMinutesFromNow.getSeconds() - 60);
+      break;
+
+    case 30:
+      twoMinutesFromNow.setSeconds(twoMinutesFromNow.getSeconds() - 90);
+
+    default:
+      break;
+  }
+}
+
+// start the timer
+function startTimer() {
+  setTimer();
+  timer = setInterval(countdown, 1000);
+}
+
+// stop the timer
+function stopTimer() {
+  clearInterval(timer);
+  win_count++;
+}
 
 try {
 
@@ -33,17 +76,16 @@ try {
 
 
   start.addEventListener("click", function (event) {
-
     event.preventDefault();
-    var interval = setInterval(startTimer, 1000);
     game.start().then(() => {
       countryName.innerHTML = "Country name: " + game.country;
       start.disabled = true;
       capital.innerHTML = game.getWordHolderText();
       guessButton.disabled = false;
     });
+
     // start the timer 
-    setInterval(startTimer, 1000)
+    startTimer();
 
   });
 
@@ -53,7 +95,10 @@ try {
     game.guess(guessInput.value);
     capital.innerHTML = game.getWordHolderText();
     guessInput.value = "";
+
     if (game.isOver == true) {
+
+      stopTimer();
       guessSubmitButton.disabled = true;
       guessInput.disabled = true;
       resetGame.style.display = "block";
@@ -75,6 +120,3 @@ try {
   console.error(error);
   alert(error);
 }
-
-
-
