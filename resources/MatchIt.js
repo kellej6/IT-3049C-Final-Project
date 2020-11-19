3529b9966a2aebaf4a583440f0d10ffd0913413f
Muhammad Moiz Hussain <mohammadmoiz6@gmail.com>
2020-11-19 18:21:10 -0500

    Continue the game untill the timer is finished

--- resources/MatchIt.js
+++ resources/MatchIt.js
@@ -1,390 +1,498 @@
 class MatchIt {
-    constructor(_canvas) {
-        if (!_canvas) {
-            throw new Error("invalid canvas provided");
-        }
-        this.country = "";
-        this.capital = "";
-        this.wordHolderCounter = 0;
-        this.guessCounter = 0;
-        this.guesses = new Array();
-        this.previousGuessedWord = new Array();
-        this.didWin;
-        this.gameOver;
-        this.canvas = _canvas;
-        this.ctx = this.canvas.getContext("2d");
+  constructor(_canvas) {
+    if (!_canvas) {
+      throw new Error("invalid canvas provided");
     }
+    this.country = "";
+    this.capital = "";
+    this.wordHolderCounter = 0;
+    this.score = 0;
+    this.guessCounter = 0;
+    this.guesses = new Array();
+    this.previousGuessedWord = new Array();
+    // this.didWin;
+    // this.isOver;
+    this.canvas = _canvas;
+    this.ctx = this.canvas.getContext("2d");
+  }
 
-    async getRandomCountry(isoCode) {
-        try {
-            let url = "https://restcountries.eu/rest/v2/alpha/" + isoCode;
-            return await fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    this.capital = data.capital.toLowerCase();
-                    return data.name;
-                });
-        } catch {
-            console.error(error);
-            alert(error);
-        }
+  async getRandomCountry(isoCode) {
+    try {
+      let url = "https://restcountries.eu/rest/v2/alpha/" + isoCode;
+      return await fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          this.capital = data.capital.toLowerCase();
+          return data.name;
+        });
+    } catch {
+      console.error(error);
+      alert(error);
     }
+  }
 
-    async start() {
-        const countriesIsoCode = ["AF",
-            "AX",
-            "AL",
-            "DZ",
-            "AS",
-            "AD",
-            "AO",
-            "AI",
-            "AQ",
-            "AG",
-            "AR",
-            "AM",
-            "AW",
-            "AU",
-            "AT",
-            "AZ",
-            "BS",
-            "BH",
-            "BD",
-            "BB",
-            "BY",
-            "BE",
-            "BZ",
-            "BJ",
-            "BM",
-            "BT",
-            "BO",
-            "BQ",
-            "BA",
-            "BW",
-            "BV",
-            "BR",
-            "IO",
-            "BN",
-            "BG",
-            "BF",
-            "BI",
-            "KH",
-            "CM",
-            "CA",
-            "CV",
-            "KY",
-            "CF",
-            "TD",
-            "CL",
-            "CN",
-            "CX",
-            "CC",
-            "CO",
-            "KM",
-            "CG",
-            "CD",
-            "CK",
-            "CR",
-            "CI",
-            "HR",
-            "CU",
-            "CW",
-            "CY",
-            "CZ",
-            "DK",
-            "DJ",
-            "DM",
-            "DO",
-            "EC",
-            "EG",
-            "SV",
-            "GQ",
-            "ER",
-            "EE",
-            "ET",
-            "FK",
-            "FO",
-            "FJ",
-            "FI",
-            "FR",
-            "GF",
-            "PF",
-            "TF",
-            "GA",
-            "GM",
-            "GE",
-            "DE",
-            "GH",
-            "GI",
-            "GR",
-            "GL",
-            "GD",
-            "GP",
-            "GU",
-            "GT",
-            "GG",
-            "GN",
-            "GW",
-            "GY",
-            "HT",
-            "HM",
-            "VA",
-            "HN",
-            "HK",
-            "HU",
-            "IS",
-            "IN",
-            "ID",
-            "IR",
-            "IQ",
-            "IE",
-            "IM",
-            "IL",
-            "IT",
-            "JM",
-            "JP",
-            "JE",
-            "JO",
-            "KZ",
-            "KE",
-            "KI",
-            "KP",
-            "KR",
-            "XK",
-            "KW",
-            "KG",
-            "LA",
-            "LV",
-            "LB",
-            "LS",
-            "LR",
-            "LY",
-            "LI",
-            "LT",
-            "LU",
-            "MO",
-            "MK",
-            "MG",
-            "MW",
-            "MY",
-            "MV",
-            "ML",
-            "MT",
-            "MH",
-            "MQ",
-            "MR",
-            "MU",
-            "YT",
-            "MX",
-            "FM",
-            "MD",
-            "MC",
-            "MN",
-            "ME",
-            "MS",
-            "MA",
-            "MZ",
-            "MM",
-            "NA",
-            "NR",
-            "NP",
-            "NL",
-            "AN",
-            "NC",
-            "NZ",
-            "NI",
-            "NE",
-            "NG",
-            "NU",
-            "NF",
-            "MP",
-            "NO",
-            "OM",
-            "PK",
-            "PW",
-            "PS",
-            "PA",
-            "PG",
-            "PY",
-            "PE",
-            "PH",
-            "PN",
-            "PL",
-            "PT",
-            "PR",
-            "QA",
-            "RS",
-            "RE",
-            "RO",
-            "RU",
-            "RW",
-            "BL",
-            "SH",
-            "KN",
-            "LC",
-            "MF",
-            "PM",
-            "VC",
-            "WS",
-            "SM",
-            "ST",
-            "SA",
-            "SN",
-            "CS",
-            "SC",
-            "SL",
-            "SG",
-            "SX",
-            "SK",
-            "SI",
-            "SB",
-            "SO",
-            "ZA",
-            "GS",
-            "SS",
-            "ES",
-            "LK",
-            "SD",
-            "SR",
-            "SJ",
-            "SZ",
-            "SE",
-            "CH",
-            "SY",
-            "TW",
-            "TJ",
-            "TZ",
-            "TH",
-            "TL",
-            "TG",
-            "TK",
-            "TO",
-            "TT",
-            "TN",
-            "TR",
-            "XT",
-            "TM",
-            "TC",
-            "TV",
-            "UG",
-            "UA",
-            "AE",
-            "GB",
-            "US",
-            "UM",
-            "UY",
-            "UZ",
-            "VU",
-            "VE",
-            "VN",
-            "VG",
-            "VI",
-            "WF",
-            "EH",
-            "YE",
-            "ZM",
-            "ZW"];
+  async start() {
+    const countriesIsoCode = ["AF",
+      "AX",
+      "AL",
+      "DZ",
+      "AS",
+      "AD",
+      "AO",
+      "AI",
+      "AQ",
+      "AG",
+      "AR",
+      "AM",
+      "AW",
+      "AU",
+      "AT",
+      "AZ",
+      "BS",
+      "BH",
+      "BD",
+      "BB",
+      "BY",
+      "BE",
+      "BZ",
+      "BJ",
+      "BM",
+      "BT",
+      "BO",
+      "BQ",
+      "BA",
+      "BW",
+      "BV",
+      "BR",
+      "IO",
+      "BN",
+      "BG",
+      "BF",
+      "BI",
+      "KH",
+      "CM",
+      "CA",
+      "CV",
+      "KY",
+      "CF",
+      "TD",
+      "CL",
+      "CN",
+      "CX",
+      "CC",
+      "CO",
+      "KM",
+      "CG",
+      "CD",
+      "CK",
+      "CR",
+      "CI",
+      "HR",
+      "CU",
+      "CW",
+      "CY",
+      "CZ",
+      "DK",
+      "DJ",
+      "DM",
+      "DO",
+      "EC",
+      "EG",
+      "SV",
+      "GQ",
+      "ER",
+      "EE",
+      "ET",
+      "FK",
+      "FO",
+      "FJ",
+      "FI",
+      "FR",
+      "GF",
+      "PF",
+      "TF",
+      "GA",
+      "GM",
+      "GE",
+      "DE",
+      "GH",
+      "GI",
+      "GR",
+      "GL",
+      "GD",
+      "GP",
+      "GU",
+      "GT",
+      "GG",
+      "GN",
+      "GW",
+      "GY",
+      "HT",
+      "HM",
+      "VA",
+      "HN",
+      "HK",
+      "HU",
+      "IS",
+      "IN",
+      "ID",
+      "IR",
+      "IQ",
+      "IE",
+      "IM",
+      "IL",
+      "IT",
+      "JM",
+      "JP",
+      "JE",
+      "JO",
+      "KZ",
+      "KE",
+      "KI",
+      "KP",
+      "KR",
+      "XK",
+      "KW",
+      "KG",
+      "LA",
+      "LV",
+      "LB",
+      "LS",
+      "LR",
+      "LY",
+      "LI",
+      "LT",
+      "LU",
+      "MO",
+      "MK",
+      "MG",
+      "MW",
+      "MY",
+      "MV",
+      "ML",
+      "MT",
+      "MH",
+      "MQ",
+      "MR",
+      "MU",
+      "YT",
+      "MX",
+      "FM",
+      "MD",
+      "MC",
+      "MN",
+      "ME",
+      "MS",
+      "MA",
+      "MZ",
+      "MM",
+      "NA",
+      "NR",
+      "NP",
+      "NL",
+      "AN",
+      "NC",
+      "NZ",
+      "NI",
+      "NE",
+      "NG",
+      "NU",
+      "NF",
+      "MP",
+      "NO",
+      "OM",
+      "PK",
+      "PW",
+      "PS",
+      "PA",
+      "PG",
+      "PY",
+      "PE",
+      "PH",
+      "PN",
+      "PL",
+      "PT",
+      "PR",
+      "QA",
+      "RS",
+      "RE",
+      "RO",
+      "RU",
+      "RW",
+      "BL",
+      "SH",
+      "KN",
+      "LC",
+      "MF",
+      "PM",
+      "VC",
+      "WS",
+      "SM",
+      "ST",
+      "SA",
+      "SN",
+      "CS",
+      "SC",
+      "SL",
+      "SG",
+      "SX",
+      "SK",
+      "SI",
+      "SB",
+      "SO",
+      "ZA",
+      "GS",
+      "SS",
+      "ES",
+      "LK",
+      "SD",
+      "SR",
+      "SJ",
+      "SZ",
+      "SE",
+      "CH",
+      "SY",
+      "TW",
+      "TJ",
+      "TZ",
+      "TH",
+      "TL",
+      "TG",
+      "TK",
+      "TO",
+      "TT",
+      "TN",
+      "TR",
+      "XT",
+      "TM",
+      "TC",
+      "TV",
+      "UG",
+      "UA",
+      "AE",
+      "GB",
+      "US",
+      "UM",
+      "UY",
+      "UZ",
+      "VU",
+      "VE",
+      "VN",
+      "VG",
+      "VI",
+      "WF",
+      "EH",
+      "YE",
+      "ZM",
+      "ZW"];
 
-        var randValue = (Math.random() * 253);
-        var isoCode = countriesIsoCode[Math.floor(randValue)];
-        const randomCountry = await this.getRandomCountry(isoCode);
-        this.country = randomCountry;
-        return;
-    }
+    var randValue = (Math.random() * 253);
+    var isoCode = countriesIsoCode[Math.floor(randValue)];
+    const randomCountry = await this.getRandomCountry(isoCode);
+    this.country = randomCountry;
+    this.clearCanvas();
+    return;
+  }
 
-    guess(letter) {
-        var checkCase = /^[A-Za-z]+$/;
-        try{
-          if(letter.length == 0){
-            throw new Error("Please provide a letter.");
-          } else if (!(letter.match(checkCase)) && letter == " "){
-            throw new Error("Guess should be a letter.");
-          } else if (letter.length > 1){
-            throw new Error("Please provide one letter.");
-          } else {
-            letter = letter.toLowerCase();
-            if(this.guesses.includes(letter)){
-              throw new Error ("Letter already exists, please provide a new one.");
-            } else {
-              this.guesses.push(letter);
-            }
-            if(this.capital.includes(letter)){
-              this.checkWin();
-            } else{
-              this.onWrongGuess();
-            }
-          }
+  guess(letter) {
+    var checkCase = /^[A-Za-z]+$/;
+    try {
+      if (letter.length == 0) {
+        throw new Error("Please provide a letter.");
+      } else if (!(letter.match(checkCase)) && letter == " ") {
+        throw new Error("Guess should be a letter.");
+      } else if (letter.length > 1) {
+        throw new Error("Please provide one letter.");
+      } else {
+        letter = letter.toLowerCase();
+        if (this.guesses.includes(letter)) {
+          throw new Error("Letter already exists, please provide a new one.");
+        } else {
+          this.guesses.push(letter);
         }
-        catch (error){
-          // console.error(error);
-          alert(error);
+        if (this.capital.includes(letter)) {
+          this.checkWin();
+        } else {
+          this.onWrongGuess();
         }
-    }
-
-    //This function will call only when the player will guess the wrong letter for the capital. 
-    //If the user enter the letter which includes in the capital name then the checkWin function will be called
-    onWrongGuess() {
-        this.guessCounter++;
-        if (this.guessCounter == 1) {
-            // function for draw base of hangman
-        } else if (this.guessCounter == 2) {
-            //function for draw stand
-        } else if (this.guessCounter == 3) {
-            // function for draw top
-        } else if (this.guessCounter == 4) {
-            //function for draw hook to hang hangman
-        } else if (this.guessCounter == 5) {
-            //function for draw head   
-        } else if (this.guessCounter == 6) {
-            //function for draw body
-        } else if (this.guessCounter == 7){
-            //function for draw left arm
-        } else if (this.guessCounter == 8){
-            //function for draw right arm
-        } else if (this.guessCounter == 9){
-            //function for draw left leg
-        } else if (this.guessCounter == 10){
-            //function for draw right leg
-            this.didWin = false;
-            this.gameOver = true;
-        }
-    }
-
-    checkWin(){
-        
-    }
-
-    getWordHolderText() {
-    
-        let wordHolder = "";
-        var checkCase = /^[A-Za-z]+$/;
-        for (let i = 0; i < this.capital.length; i++){
-          if(this.capital[i] == this.guesses[this.guesses.length -1]){
-            wordHolder += this.capital[i];
-          }
-          else{
-            wordHolder += "_";
-          }
-        }
-    
-        if(this.wordHolderCounter == 0){
-          this.previousGuessedWord.push(wordHolder);
-          this.wordHolderCounter++;
-        }
-    
-        let previousGuessed = this.previousGuessedWord[this.previousGuessedWord.length - 1].split("");
-        let i = 0;
-        while(i < this.capital.length){
-          if(previousGuessed[i] == "_" && wordHolder[i].match(checkCase)){
-            previousGuessed[i] = wordHolder[i];
-          }
-          i++;
-        }
-        this.previousGuessedWord[this.previousGuessedWord.length - 1] = previousGuessed.join("");
-        return previousGuessed.join("");
-    
       }
+    }
+    catch (error) {
+      // console.error(error);
+      alert(error);
+    }
+  }
 
+  //This function will call only when the player will guess the wrong letter for the capital. 
+  //If the user enter the letter which includes in the capital name then the checkWin function will be called
+  onWrongGuess() {
+    this.guessCounter++;
+    switch (this.guessCounter) {
+      case 1:
+        // function for draw base of hangman
+        this.drawBase();
+        break;
+      case 2:
+        //function for draw stand
+        this.drawMainBeam();
+        break;
+      case 3:
+        // function for draw top
+        this.drawTop();
+        break;
+      case 4:
+        //function for draw hook to hang hangman
+        this.drawNoose();
+        break;
+      case 5:
+        //function for draw head
+        this.drawHead();
+        break;
+      case 6:
+        //function for draw body
+        this.drawBody();
+        break;
+      case 7:
+        //function for draw left arm
+        this.drawLeftArm();
+        break;
+      case 8:
+        //function for draw right arm
+        this.drawRightArm();
+        break;
+      case 9:
+        //function for draw left leg
+        this.drawLeftLeg();
+        break;
+      case 10:
+        //function for draw right leg
+        this.drawRightLeg();
+        this.userGuessedCapital = false;
+        this.isOver = true;
+        break;
+    }
+  }
+
+  checkWin() {
+    const guessedWord = this.getWordHolderText();
+    if (guessedWord === this.capital) {
+      this.score++;
+      this.userGuessedCapital = true;
+      this.isOver = true;
+    }
+  }
+
+  getWordHolderText() {
+
+    let wordHolder = "";
+    var checkCase = /^[A-Za-z]+$/;
+    for (let i = 0; i < this.capital.length; i++) {
+      if (this.capital[i] == this.guesses[this.guesses.length - 1]) {
+        wordHolder += this.capital[i];
+      }
+      else {
+        wordHolder += "_";
+      }
+    }
+
+    if (this.wordHolderCounter == 0) {
+      this.previousGuessedWord.push(wordHolder);
+      this.wordHolderCounter++;
+    }
+
+    let previousGuessed = this.previousGuessedWord[this.previousGuessedWord.length - 1].split("");
+    let i = 0;
+    while (i < this.capital.length) {
+      if (previousGuessed[i] == "_" && wordHolder[i].match(checkCase)) {
+        previousGuessed[i] = wordHolder[i];
+      }
+      i++;
+    }
+    this.previousGuessedWord[this.previousGuessedWord.length - 1] = previousGuessed.join("");
+    return previousGuessed.join("");
+
+  }
+
+  resetGameData() {
+    this.country = "";
+    this.capital = "";
+    this.wordHolderCounter = 0;
+    this.guessCounter = 0;
+    this.guesses = [];
+    this.previousGuessedWord = [];
+    this.isOver = false;
+    this.userGuessedCapital = false;
+  }
+
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  drawBase() {
+    this.ctx.fillStyle = 'white';
+    this.ctx.fillRect(10, 290, 175, 10); // Base 
+  }
+
+  drawMainBeam() {
+    this.ctx.fillStyle = 'white';
+    this.ctx.fillRect(95, 10, 10, 280);
+
+  }
+
+  drawNoose() {
+    this.ctx.fillStyle = 'white';
+    this.ctx.fillRect(245, 10, 10, 50);
+
+  }
+
+  drawTop() {
+    this.ctx.fillStyle = 'white';
+    this.ctx.fillRect(95, 10, 150, 10);
+
+  }
+
+  drawHead() {
+    this.ctx.fillStyle = 'white';
+    this.ctx.beginPath();
+    this.ctx.arc(250, 100, 40, 0, 2 * Math.PI);
+    this.ctx.stroke();
+
+  }
+
+  drawBody() {
+    //var ctx = canvas.getContext("2d");
+    this.ctx.beginPath();
+    this.ctx.moveTo(250, 140);
+    this.ctx.lineTo(250, 270);
+    this.ctx.stroke();
+  }
+
+  drawLeftArm() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(250, 140);
+    this.ctx.lineTo(170, 200);
+    this.ctx.stroke();
+  }
+
+  drawRightArm() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(250, 140);
+    this.ctx.lineTo(340, 200);
+    this.ctx.stroke();
+  }
+
+  drawLeftLeg() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(250, 270);
+    this.ctx.lineTo(170, 390);
+    this.ctx.stroke();
+  }
+
+  drawRightLeg() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(250, 270);
+    this.ctx.lineTo(320, 390);
+    this.ctx.stroke();
+  }
 }
-
--- resources/index.js
+++ resources/index.js
@@ -4,64 +4,131 @@ let countryName = document.getElementById("country");
 let guessButton = document.getElementById("guessSubmitButton");
 let playAgainButton = document.getElementById("resetGame");
 let capital = document.getElementById("capital");
+let gameScore = document.getElementById("gameScore");
 
-let countDownDate = new Date();
-countDownDate.setMinutes(countDownDate.getMinutes() + 2);
+let timer;  // the timer function
+let twoMinutesFromNow; // the time two minutes from now
+let win_count = 0; //  the count of times the player has won
+let distance;
 
+let game = new MatchIt(canvas)
+
+function countdown() {
+  // the time as of right now
+  let rightnow = new Date().getTime();
+
+  // the time differential between right now and two minutes from now
+  distance = twoMinutesFromNow - rightnow;
+  console.log(distance);
+
+  // the time expansion into units of time
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // format the time that will display on the board
+  let text = minutes + "m " + seconds + "s ";
+  document.getElementById("timer").innerHTML = text;
+
+  // if the timer runs out, display "expired" and stop the timer
+  if (distance < 0) {
+    document.getElementById("timer").innerHTML = "EXPIRED";
+    clearInterval(timer);
+    playAgainButton.disabled = false;
+    guessSubmitButton.disabled = true;
+    guessInput.disabled = true;
+    gameScore.innerHTML = "Your Score is: " + game.score;
+  }
+}
+
+// set the variable "twoMinutesFromNow" to two minutes and two seconds from right now
+function setTimer() {
+  twoMinutesFromNow = new Date();
+  twoMinutesFromNow.setMinutes(twoMinutesFromNow.getMinutes() + 2);
+  twoMinutesFromNow.setSeconds(twoMinutesFromNow.getSeconds() + 2);
+
+  console.log(win_count);
+
+  // decrement the timer by 30 seconds for each 10 additional wins
+  switch (win_count) {
+    case 10:
+      twoMinutesFromNow.setSeconds(twoMinutesFromNow.getSeconds() - 30);
+      break;
+    case 20:
+      twoMinutesFromNow.setSeconds(twoMinutesFromNow.getSeconds() - 60);
+      break;
+    case 30:
+      twoMinutesFromNow.setSeconds(twoMinutesFromNow.getSeconds() - 90);
+    default:
+      break;
+  }
+}
+
+// start the timer
 function startTimer() {
-    let now = new Date().getTime();
-    let timeDistance = countDownDate - now;
+  setTimer();
+  timer = setInterval(countdown, 1000);
+}
 
-    var minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeDistance % (1000 * 60)) / 1000);
-
-    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s";
-
-    if (timeDistance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "EXPIRED";
-        playAgainButton.disabled = false;
-        guessButton.disabled = true;
-    }
-};
+// stop the timer
+function stopTimer() {
+  clearInterval(timer);
+}
 
 try {
 
-    let game = new MatchIt(canvas)
+  start.addEventListener("click", function (event) {
+    event.preventDefault();
+    startFunction();
+    
+    // start the timer
+    startTimer();
 
-    start.addEventListener("click", function (event) {
-        event.preventDefault();
-        game.start().then(() => {
-            countryName.innerHTML = "Country name: " + game.country;
-            start.disabled = true;
-            capital.innerHTML = game.getWordHolderText();
-            guessButton.disabled = false;
-        });
+  });
 
-        // start the timer
-        setInterval(startTimer, 1000);
+  function startFunction(){
+    game.start().then(() => {
+      countryName.innerHTML = "Country name: " + game.country;
+      start.disabled = true;
+      capital.innerHTML = game.getWordHolderText();
+      guessButton.disabled = false;
+      playAgainButton.disabled=true;
     });
+  }
 
-    guessForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-        guessInput = document.getElementById("guessInput");
-        game.guess(guessInput.value);
-        capital.innerHTML = game.getWordHolderText();
-        guessInput.value = "";
-        if(game.isOver == true){
-          guessButton.disabled = true;
-          guessInput.disabled = true;
-          resetGame.style.display = "block";
-          if(game.didWin == true){
-            alert("Congratulations! You won.");
-          } else{
-            alert("Game Lost.");
-          }
-        }
-      });
+  guessForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    guessInput = document.getElementById("guessInput");
+    game.guess(guessInput.value);
+    capital.innerHTML = game.getWordHolderText();
+    guessInput.value = "";
+    if(game.isOver == true){
+      if(game.userGuessedCapital == true && distance > 0){
+        game.resetGameData();
+        capital.innerHTML = "";
+        countryName.innerHTML = "Country name: ";
+        win_count++;
+        gameScore.innerHTML = "Your Score is: " + game.score;
+        startFunction();
+      } else if (game.userGuessedCapital == false){
+        guessSubmitButton.disabled = true;
+        guessInput.disabled = true;
+        playAgainButton.disabled = false;
+        gameScore.innerHTML = "Your Score is: " + game.score;
+        stopTimer();
+        alert("Unfortunately! You did not guess.")
+      }
+    }
+  });
 
+  function resetGame() {
+    location.reload();
+  }
 
 } catch (error) {
-    console.error(error);
-    alert(error);
+
+  console.error(error);
+  alert(error);
 }
+
+
+
